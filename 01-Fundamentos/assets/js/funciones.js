016eefd6001cbbed6a299ed0c0e3be057256d3dc
Angel Abraham <angel-07061@hotmail.com>
2020-11-26 16:36:36 -0600

    Funciones de flechas simplificadas

--- 01-Fundamentos/assets/js/funciones.js
+++ 01-Fundamentos/assets/js/funciones.js
@@ -19,7 +19,7 @@ function saludar( nombre) {
 }
 
 // saludar('Abraham');
-saludar('Abraham', 40,true, 'México');
+// saludar('Abraham', 40,true, 'México');
 
 //Probar argumentos con funciones anonimas
 const saludar2 = function (nombre) {
@@ -42,3 +42,45 @@ const saludarFlecha2 = (nombre) =>{
 
 saludarFlecha2('Abraham');
 
+
+//retornar valores
+
+function saludar( nombre) {
+    console.log(arguments);
+    console.log('Hola ' + nombre);
+    return 1;
+    // return [1,2]; //Manera correcta
+    // return 1,2;
+    //Nunca se va a ejecutar
+    // console.log("Soy un código que esta despues detl return");
+}
+
+const retornoDeSaludar = saludar('Abraham', 40,true, 'México');
+console.log({retornoDeSaludar});
+
+//Otra manera de retornar
+function sumar(a,b){
+    return a+b;
+}
+
+console.log(sumar(1,2));
+
+//Funcion de fleshas con 2 constantes
+// const sumar2= (a,b)=>{
+//     return a+b;
+// }
+
+//resumida de funcion de flechas
+const sumar2= (a,b)=> a+b;
+console.log(sumar2(1,2));
+
+function getAleatorio(){
+    return Math.random();
+}
+
+console.log(getAleatorio());
+
+//Tarea de taller
+
+const getAleatorio2 = () => Math.random(); 
+console.log(getAleatorio());
