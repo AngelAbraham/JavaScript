9bc1957c848aecb2b36f967486d809acf2e05c90
Angel Abraham <angel-07061@hotmail.com>
2020-11-26 14:52:44 -0600

    Commit arreglos

---
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,28 @@
+// forma no tipica
+// const arr =  new Array[10];
+
+//forma tipica de contruir arreglos
+// const arr= [];
+// console.log(arr);
+
+let videoJuegos = ['Mario 3','Megaman','Chrono Trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+
+let arregloCosas= [
+                    true,
+                    123,
+                    'Abraham',
+                    1 + 2,
+                    function(){},
+                    ()=>{},
+                    {a :1},
+                    ['X','Megaman','Zero', 'Dr.Light',['Willy',
+                                                        'Woodman'
+                                                            ]]        ];
+
+// console.log({arregloCosas});
+// console.log(arregloCosas[2]);
+
+console.log(arregloCosas[7][4][1]);
