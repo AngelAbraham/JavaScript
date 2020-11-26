36b564cc433526e1542000bba9fa731e16122399
Angel Abraham <angel-07061@hotmail.com>
2020-11-26 13:36:22 -0600

    Commit 01 js

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,2 @@
+//Demostracion de mala practica de programacion con tipos de variables 'VAR' (var miNombre='Abraham';)
+//console.log(miNombre);
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,52 @@
+// console.log('Hola mundo');
+
+// tipos de variables const,var let
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a+b;
+
+// console.log('%c Mis variables','color:blue; font-weight:bold');
+
+// console.log( {a} );
+// console.log( {b} );
+// console.log( {c} );
+
+// console.log( 'a',a );
+// console.log( 'b',b );
+// console.log( 'c',c );
+
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+
+alert('Hola desde app.js');
+let a=10,
+    b=20,
+    c='Hola ',
+    d='Spiderman',
+    x = a+b;
+
+const saludo = c + d;
+
+c='Hola de nuevo';
+
+// console.table({a,b,c,d});
+
+//palabra reservada
+
+//Problemas con var nunca utilizarla por malas practicas
+// var miNombre= 'Abraham';
+
+// var outerWidth= 100000 ;
+// var outerHeight= 600 ;
+
+// let outerWidth= 100000 ;
+// const outerHeight= 600 ;
+
+console.log(miNuevoNombre);
+
+//var miNuevoNombre= 'Angel Abraham';
+let miNuevoNombre= 'Angel Abraham';
