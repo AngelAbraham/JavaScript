fe5d0fdbe6a1902e17dd1ac43e375f50f6eb4b8d
Angel Abraham <angel-07061@hotmail.com>
2020-12-01 21:11:14 -0600

    Commit if else con objetos

--- 01-Fundamentos/assets/js/if-else.js
+++ 01-Fundamentos/assets/js/if-else.js
@@ -35,3 +35,17 @@ if(dia === 0){  //importar el tipo de datos
     //     console.log('Lunes');
     // }
     // console.log('No es domingo');
+
+
+    //sin usar if else, o swith unicamente objetos
+
+    dia= 3; //0:domingo
+
+    const diaLetras= {
+        0:'Domingo', 1:'Lunes', 2:'Martes', 3:'Miercoles', 4:'Jueves', 5:'Viernes',6:'Sabado',
+    }
+    // console.log(diaLetras[dia] || 'Dia no definido');
+
+    const diaLetras2= ['Domingo', 'Lunes','Martes', 'Miercoles', 'Jueves', 'Viernes','Sabado']
+
+    console.log(diaLetras2[dia]);
