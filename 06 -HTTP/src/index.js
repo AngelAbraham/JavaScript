824085cb5e504f9167c30dffc9f8fab0b2a64508
Angel Abraham <angel-07061@hotmail.com>
2020-12-11 13:03:26 -0600

    Commit generar usuarios Crud

--- 06 -HTTP/src/index.js
+++ 06 -HTTP/src/index.js
@@ -1,9 +1,10 @@
 // import { obtenerChiste} from './js/http-provide';
 // obtenerChiste().then(console.log);
-
-import {init} from "./js/usuarios-page";
-init();
-
+// import {init} from "./js/usuarios-page";
+// init();
 // import { obtenerUsuarios} from './js/http-provide';
-
 // obtenerUsuarios().then(console.log);
+
+import * as CRUD from './js/crud-provider';
+
+CRUD.getUsuario(2).then(console.log);
